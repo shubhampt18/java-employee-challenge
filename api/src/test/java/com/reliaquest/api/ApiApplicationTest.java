17c6771be776e429dfc4b60f175776d2d0ddba57
Shubham Patil <shubhampt34782@gmail.com>
2024-12-13 12:30:20 +0530

    Added test

--- api/src/test/java/com/reliaquest/api/ApiApplicationTest.java
+++ api/src/test/java/com/reliaquest/api/ApiApplicationTest.java
@@ -1,13 +1,27 @@
 package com.reliaquest.api;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
 class ApiApplicationTest {
 
+    @Autowired
+    public WebApplicationContext applicationContext;
+
+    public MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(applicationContext).build();
+    }
+
     @Test
-    void someTest() {
-        // do the thing here
+    void contextLoads() {
     }
 }
